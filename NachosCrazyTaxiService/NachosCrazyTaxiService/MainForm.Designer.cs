f22987e8fd747172a54ed4ba1533efe2a8343f26
Nic Bunting <steelmagnum@hotmail.com>
2016-07-10 12:11:30 -0700

    Initial commit

---
+++ NachosCrazyTaxiService/NachosCrazyTaxiService/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace NachosCrazyTaxiService
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.MainDisplay = new System.Windows.Forms.PictureBox();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.AutoScroll = true;
+            this.MainPanel.Controls.Add(this.MainDisplay);
+            this.MainPanel.Location = new System.Drawing.Point(12, 12);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(260, 237);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // MainDisplay
+            // 
+            this.MainDisplay.Location = new System.Drawing.Point(36, 3);
+            this.MainDisplay.Name = "MainDisplay";
+            this.MainDisplay.Size = new System.Drawing.Size(221, 231);
+            this.MainDisplay.TabIndex = 0;
+            this.MainDisplay.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.MainPanel);
+            this.Name = "MainForm";
+            this.Text = "Nacho\'s Crazy Taxi Service";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MainDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.PictureBox MainDisplay;
+    }
+}
+
---
+++ NachosCrazyTaxiService/NachosCrazyTaxiService/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace NachosCrazyTaxiService
+{
+    static class Program
+    {
+        private static List<Entity> allEntities { get; set; }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+
+        public static void GenerateWorld(int width, int height)
+        {
+            int seed = 0;
+            GenerateWorld(width, height, seed);
+        }
+
+        public static void GenerateWorld(int width, int height, int seed)
+        {
+            allEntities = new List<Entity>();
+
+            Entity box = new Entity(50, 50, 30, 30, Color.Black, 1);
+            allEntities.Add(box);
+
+            for (int i = 0; i < 2; i++)
+            {
+                box.TakeDamage(5, i);   //debug
+            }
+        }
+
+        public static void DrawWorld(Bitmap image, Color lineColour, int lineThickness)
+        {
+            if (image == null)
+            {
+                return;
+            }
+
+            using (Graphics graphics = Graphics.FromImage(image))
+            {
+                // Fill image with white
+                Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+                graphics.FillRectangle(Brushes.White, rect);
+
+                // Draw all grids
+                foreach (Entity entity in allEntities)
+                {
+                    entity.Draw(graphics);
+                }
+            }
+        }
+    }
+}
